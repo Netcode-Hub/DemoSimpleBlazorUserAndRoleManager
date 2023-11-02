7bd911b5e68fd0ebe8d702fc87138f713b31be68
Netcode-Hub <Rastinga1995@hotmail.com>
2023-11-02 00:52:39 +0000

    .NET Blazor  Server | Change User Role Or Delete User Account Completely in Default Authentication .

---
+++ DemoSimpleBlazorUserAndRoleManager/UserDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace DemoSimpleBlazorUserAndRoleManager
+{
+    public class UserDetails
+    {
+        public string? UserName { get; set; }
+        public string? Role { get; set; }
+    }
+}
